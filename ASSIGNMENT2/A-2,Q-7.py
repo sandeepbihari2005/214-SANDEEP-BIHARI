7fd0ac558096afaf26746f5f40d05c125cb60731
Sandeep Kumar Bihari <sandeepbihari2005@gmail.com>
2026-08-04 17:20:47 +0530

    Add files via upload

---
+++ ASSIGNMENT2/A-2,Q-7.py
@@ -0,0 +1,15 @@
+import numpy as np
+import statistics as stats
+data = [10, 15, 20, 25, 30, 40, 45, 50, 55]
+
+mean = np.mean(data)
+median = np.median(data)
+mode = stats.mode(data)
+variance = np.var(data)
+std = np.std(data)
+
+print(f"Mean: {mean}")
+print(f"Median: {median}")
+print(f"Mode: {mode}")
+print(f"Variance: {variance}")
+print(f"Standard Deviation: {std}")
